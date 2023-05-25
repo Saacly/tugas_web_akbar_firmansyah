92b62c676bf5813f1675ec91c9762cb196b97c03
Saacly <akbarfirmansyah1085@gmail.com>
2023-05-25 13:20:48 +0700

    first commit

--- Minggu_7/resources/views/backend/layouts/sidebar.blade.php
+++ Minggu_7/resources/views/backend/layouts/sidebar.blade.php
@@ -175,7 +175,7 @@
 
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                     <li class="dropdown-header">
-                        <h6>Rafliqy Havies Ahimza</h6>
+                        <h6>Akbar Firmansyah</h6>
                         <span>Web Designer</span>
                     </li>
                     <li>
